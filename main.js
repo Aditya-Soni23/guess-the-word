2a50c4f5970db2dcf924c3f2d6c1b82a82f88198
Aditya-Soni23 <77341618+Aditya-Soni23@users.noreply.github.com>
2022-04-22 21:46:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,141 @@
+function start(){
+    window.location = "1stq.html";
+}
+
+function btn(){
+
+var ans = document.getElementById("ip").value;
+
+ if(ans == "coding"){
+   window.location = "2ndq.html";
+ }
+ else{
+   window.location = "index.html";
+ }
+}
+
+
+
+function btu(){
+
+  var ans = document.getElementById("iu").value;
+  
+   if(ans == "car"){
+     window.location = "3rdq.html";
+   }
+   else{
+     window.location = "index.html";
+   }
+  }
+
+
+
+  function bts(){
+
+    var ans = document.getElementById("ic").value;
+    
+     if(ans == "mobile"||"phone"||"mobile phone"){
+       window.location = "4thq.html";
+     }
+     else{
+       window.location = "index.html";
+     }
+    }
+
+
+
+    function btj(){
+
+      var ans = document.getElementById("io").value;
+      
+       if(ans == "water"){
+         window.location = "5thq.html";
+       }
+       else{
+         window.location = "index.html";
+       }
+      }
+
+
+
+      function btk(){
+
+        var ans = document.getElementById("ii").value;
+        
+         if(ans == "camera"){
+           window.location = "6thq.html";
+         }
+         else{
+           window.location = "index.html";
+         }
+        }
+
+
+
+        function btp(){
+
+          var ans = document.getElementById("is").value;
+          
+           if(ans == "ac"||"a/c"||"air conditioner"){
+             window.location = "7thq.html";
+           }
+           else{
+             window.location = "index.html";
+           }
+          }
+
+
+
+          function btb(){
+
+            var ans = document.getElementById("ib").value;
+            
+             if(ans == "skyscraper"){
+               window.location = "8thq.html";
+             }
+             else{
+               window.location = "index.html";
+             }
+            }
+
+
+
+            function btl(){
+
+              var ans = document.getElementById("in").value;
+              
+               if(ans == "barber"||"hair cut"){
+                 window.location = "9thq.html";
+               }
+               else{
+                 window.location = "index.html";
+               }
+              }
+
+
+
+              function bto(){
+
+                var ans = document.getElementById("ig").value;
+                
+                 if(ans == "electricity"||"turbine"||"wind turbine"){
+                   window.location = "10thq.html";
+                 }
+                 else{
+                   window.location = "index.html";
+                 }
+                }
+
+
+
+                function bt(){
+
+                  var ans = document.getElementById("i").value;
+                  
+                   if(ans == "phone"||"mobile"||"call"||"mobile phone"){
+                     window.location = "thank.html";
+                   }
+                   else{
+                     window.location = "index.html";
+                   }
+                  }
